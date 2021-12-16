2bc5314ca43119ac5d199933fad9b9392c666445
wahyubagus <wahyu.sulaksono@ist.id>
2021-12-17 00:19:03 +0700

    feature: selected indicator

--- app/src/main/java/com/wahyubagus/movieapp/HomeActivity.java
+++ app/src/main/java/com/wahyubagus/movieapp/HomeActivity.java
@@ -6,15 +6,19 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
 import com.wahyubagus.movieapp.adapters.SliderPagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
 
     private List<Slide> slideList;
     private ViewPager sliderPager;
+    private TabLayout indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +26,42 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         sliderPager = findViewById(R.id.slider_pager);
+        indicator = findViewById(R.id.indicator);
 
         // prepare list of slides
         slideList = new ArrayList<>();
-        slideList.add(new Slide(R.drawable.thebatman, "The Batman"));
-        slideList.add(new Slide(R.drawable.spiderverse, "Spiderverse"));
+        slideList.add(new Slide(R.drawable.thebatman, "The Batman - (2022)"));
+        slideList.add(new Slide(R.drawable.spiderverse, "Spider-Man: into the spider verse - (2017)"));
 
         SliderPagerAdapter adapter = new SliderPagerAdapter(this, slideList);
         sliderPager.setAdapter(adapter);
+
+        // setup time
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(), 4000, 6000);
+
+
+        indicator.setupWithViewPager(sliderPager, true);
+
     }
+
+    class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+
+            HomeActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (sliderPager.getCurrentItem() < slideList.size() - 1){
+                        sliderPager.setCurrentItem(sliderPager.getCurrentItem() + 1);
+                    } else {
+                        sliderPager.setCurrentItem(0);
+                    }
+                }
+            });
+
+        }
+    }
+
 }
